4318565374406305241919c78f9672380ed5085f
Purvansh Singh <purvanshsingh@gmail.com>
2021-06-15 06:14:26 +0530

    saving API_Doc in serialized format & main.py updated

--- creditrisk_poc/main.py
+++ creditrisk_poc/main.py
@@ -1,4 +1,3 @@
-from API_Doc.APIDoc import doc
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from hydrus.app_factory import app_factory
@@ -7,12 +6,18 @@ from hydra_python_core import doc_maker
 from hydrus.data import doc_parse
 from hydrus.data.db_models import Base, create_database_tables
 import configparser
+from hydrus.socketio_factory import create_socket
+import pickle
+
 # Fetching database_url & API_Doc path from config file
 config = configparser.ConfigParser()
 config.read('config.ini')
-
 DB_URL = config['Database']['database_url']
-API_DOC = config['API_Doc']['API_Doc_path']
+API_DOC_PATH = config['API_Doc']['API_Doc_path']
+
+# loading serialized API_Doc object
+API_Doc_object = open(API_DOC_PATH, "rb")
+doc = pickle.load(API_Doc_object)
 
 HYDRUS_SERVER_URL = "http://localhost:8080/"
 API_NAME = "creditrisk_api"
@@ -29,8 +34,10 @@ Base.metadata.create_all(engine)
 
 session = sessionmaker(bind=engine)()
 
+
 if __name__ == "__main__":
     app = app_factory(API_NAME)
+    socketio = create_socket(app, session)
     # Set the name of the API
     with set_api_name(app, API_NAME):
        # Set the API Documentation
@@ -39,5 +46,6 @@ if __name__ == "__main__":
            with set_hydrus_server_url(app, HYDRUS_SERVER_URL):
                # Set the Database session
                with set_session(app, session):
+                   socketio.run(app=app,debug=True,port=8080)
                    # Start the hydrus app
-                   app.run(host='127.0.0.1', debug=True, port=8080)
+                   #app.run(host='127.0.0.1', debug=True, port=8080)
