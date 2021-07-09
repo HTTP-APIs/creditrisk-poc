7769962ad35a79ec7379d4f35b19f190744ed18d
Purvansh Singh <purvanshsingh@gmail.com>
2021-07-09 13:45:43 +0530

    Owl -> Jsonld automation script created. (#19)
    * NPLVocab Parser added.
    
    * docwriter & NPL vocab updated
    
    * mock_portfolio_generator.py refactored.
    
    * NPL Vocab file path fixed.
    
    * Vocab_generator (Owl to jsonld) added.
    
    * NPLVocab_parser.py modified.
    
    * doc_writer modified.
    
    * updated ApiDoc.
    
    * counterparty typo update
    
    * jsonld parser renamed
    
    * import fixed

---
+++ creditrisk_poc/npl_vocab/vocab_generator.py
@@ -0,0 +1,50 @@
+"""
+This script will generate the NonPerformingLoan.jsonld from the owl ontology.
+"""
+import json
+import rdflib
+
+
+def context() -> dict:
+    """
+    Returns @context of the jsonld.
+    :return:
+    """
+    context = {
+        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        "owl": "http://www.w3.org/2002/07/owl#",
+        "xsd": "https://www.w3.org/TR/xmlschema-2/#",
+        "defines": {
+            "@reverse": "http://www.w3.org/2000/01/rdf-schema#isDefinedBy"
+        },
+        "propertyOf": {
+            "@id": "rdfs:domain",
+            "@type": "@id"
+        },
+        "propertyOn": {
+            "@id": "rdfs:range",
+            "@type": "@id"
+        }
+    }
+    return context
+
+
+def generate_jsonld(file_path: str, export_file_name: str):
+    """
+    Returns jsonld converted vocabulary from the owl ontology.
+    """
+    ontology = rdflib.Graph()
+    # loading ontology in XML format
+    ontology.parse(file_path)
+    # Converting to jsonld using Context function
+    npl_jsonld = ontology.serialize(format='json-ld', context=context(), indent=4)
+    # converting binary string to json object
+    npl_jsonld = json.loads(npl_jsonld)
+    # Writing json sting to file
+    export_file = open(export_file_name, "w")
+    npl_jsonld = json.dumps(npl_jsonld, indent=4)
+    export_file.write(npl_jsonld)
+
+
+if __name__ == '__main__':
+    generate_jsonld("NonPerformingLoan.owl", "NonPerformingLoan.jsonld")
