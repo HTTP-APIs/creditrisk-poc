7d91861971812261d5aa83f169698c396f93aeb1
Purvansh Singh <purvanshsingh@gmail.com>
2021-08-08 19:52:20 +0530

    api_name in url fixed

--- tests/mock_portfolio_generator.py
+++ tests/mock_portfolio_generator.py
@@ -19,51 +19,53 @@ def get_api_doc(apidoc_file_path: str = "ApiDoc.jsonld") -> HydraDoc:
     API_NAME = os.environ["API_NAME"] if "API_NAME" in dict(os.environ).keys() else "creditrisk_api"
     apidoc = json.load(doc_file)
     apidoc = doc_maker.create_doc(apidoc, "http://localhost:8080/", API_NAME)
-    return apidoc
+    return apidoc, API_NAME
 
 
-def generate_borrowers(apidoc: HydraDoc):
+def generate_borrowers(apidoc: HydraDoc, API_NAME: str):
     """
     Generates 100 Borrowers with no loans & no collateral
     """
     for borrower in range(1000):
         dummy_object = gen_dummy_object("Borrower", apidoc)
-        put_request = requests.put("http://localhost:8080/creditrisk_api/Borrower/", json=dummy_object)
+        put_request = requests.put("http://localhost:8080/{api_name}/Borrower/".format(api_name=API_NAME), json=dummy_object)
 
 
-def generate_private_borrowers(apidoc: HydraDoc):
+def generate_private_borrowers(apidoc: HydraDoc, API_NAME: str):
     """
     Generates 100 Borrowers with no loans & no collateral
     """
     for borrower in range(1000):
         dummy_object = gen_dummy_object("PrivateBorrower", apidoc)
-        put_request = requests.put("http://localhost:8080/creditrisk_api/PrivateBorrower/", json=dummy_object)
-        print(put_request.text)
+        put_request = requests.put("http://localhost:8080/{api_name}/PrivateBorrower/".format(api_name=API_NAME),
+                                   json=dummy_object)
 
 
-def generate_residential_mortgage(apidoc: HydraDoc):
+def generate_residential_mortgage(apidoc: HydraDoc, API_NAME: str):
     """
     Generate 1000 Borrowers with loan & no collateral
     """
     for mortgage in range(100):
         # Creating ResidentialMortgage Object
         mortgage_dummy_object = gen_dummy_object("ResidentialMortgage", apidoc)
-        put_request = requests.put("http://localhost:8080/creditrisk_api/ResidentialMortgage/", json=mortgage_dummy_object)
+        put_request = requests.put("http://localhost:8080/{api_name}/ResidentialMortgage/".format(api_name=API_NAME),
+                                   json=mortgage_dummy_object)
 
-def generate_collateral(apidoc: HydraDoc):
+def generate_collateral(apidoc: HydraDoc, API_NAME: str):
     """
     Generate 1000 collateral objects
     """
     for collateral in range(100):
         # Creating ResidentialMortgage Object
         mortgage_dummy_object = gen_dummy_object("Collateral", apidoc)
-        put_request = requests.put("http://localhost:8080/creditrisk_api/Collateral/", json=mortgage_dummy_object)
+        put_request = requests.put("http://localhost:8080/{api_name}/Collateral/".format(api_name=API_NAME),
+                                   json=mortgage_dummy_object)
 
 
 
 if __name__ == "__main__":
-    doc = get_api_doc()
-    generate_borrowers(doc)
-    generate_private_borrowers(doc)
-    generate_residential_mortgage(doc)
-    generate_collateral(doc)
+    doc, api_name = get_api_doc()
+    generate_borrowers(doc, api_name)
+    generate_private_borrowers(doc, api_name)
+    generate_residential_mortgage(doc, api_name)
+    generate_collateral(doc, api_name)
