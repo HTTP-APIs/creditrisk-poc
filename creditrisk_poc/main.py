0a2f29ba57893b0056cab65affb0568615cc4da8
Hasan Faraz Khan <farazkhan138@gmail.com>
2021-08-09 14:15:10 +0530

    Dockerize creditrisk_poc (#25)
    * changes in main.py
    
    * added gitignore
    
    * added dockerfile
    
    * docker-compose postgres
    
    * apidoc path from env

--- creditrisk_poc/main.py
+++ creditrisk_poc/main.py
@@ -1,26 +1,49 @@
+import os
+import json
+import logging
+from pathlib import Path
+from os.path import abspath, dirname
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from hydrus.app_factory import app_factory
-from hydrus.utils import set_session, set_doc, set_hydrus_server_url, set_api_name
+from hydrus.utils import (
+    set_session,
+    set_doc,
+    set_hydrus_server_url,
+    set_token,
+    set_api_name,
+    set_authentication,
+)
 from hydra_python_core import doc_maker
 from hydrus.data import doc_parse
+from gevent.pywsgi import WSGIServer
 from hydrus.data.db_models import Base, create_database_tables
 from hydrus.extensions.socketio_factory import create_socket
-import configparser
-import json
 
-# Fetching database_url & api_doc path from config file
-config = configparser.ConfigParser()
-config.read('config.ini')
-DB_URL = config['Database']['database_url']
-API_DOC_PATH = config['API_Doc']['API_Doc_path']
+
+logger = logging.getLogger(__file__)
+
+# Fetching database_url & api_doc path from env variables
+PORT = int(os.environ["PORT"]) if "PORT" in dict(os.environ).keys() else 8080
+API_NAME = os.environ["API_NAME"] if "API_NAME" in dict(os.environ).keys() else "creditrisk_api"
+DB_URL = (
+    os.environ["DB_URL"]
+    if "DB_URL" in dict(os.environ).keys()
+    else "sqlite:///database.db"
+)
+
+HYDRUS_SERVER_URL = f"http://localhost:{PORT}/"
+
+cwd_path = Path(dirname(dirname(abspath(__file__))))
+try:
+    apidoc_env = os.environ["APIDOC_REL_PATH"]
+    apidoc_path = cwd_path / Path(apidoc_env)
+except KeyError:
+    apidoc_path = cwd_path / "creditrisk_poc" / "api_doc" / "ApiDoc.jsonld"
 
 # loading serialized api_doc object
-doc_file = open(API_DOC_PATH, "r")
+doc_file = open(apidoc_path, "r")
 doc = json.load(doc_file)
-
-HYDRUS_SERVER_URL = "http://localhost:8080/"
-API_NAME = "creditrisk_api"
 
 apidoc = doc_maker.create_doc(doc, HYDRUS_SERVER_URL, API_NAME)
 
@@ -34,16 +57,30 @@ Base.metadata.create_all(engine)
 
 session = sessionmaker(bind=engine)()
 
+AUTH = False
+TOKEN = False
 
-if __name__ == "__main__":
-    app = app_factory(API_NAME)
-    socketio = create_socket(app, session)
-    # Set the name of the API
-    with set_api_name(app, API_NAME):
-       # Set the API Documentation
-       with set_doc(app, apidoc):
-           # Set HYDRUS_SERVER_URL
-           with set_hydrus_server_url(app, HYDRUS_SERVER_URL):
-               # Set the Database session
-               with set_session(app, session):
-                   socketio.run(app=app,debug=True,port=8080)
+# Create a Hydrus app
+app = app_factory(API_NAME)
+socketio = create_socket(app, session)
+
+# Set the API Documentation
+# Set HYDRUS_SERVER_URL
+# Set the Database session
+with set_authentication(app, AUTH), set_token(app, TOKEN), set_api_name(
+    app, API_NAME
+), set_doc(app, apidoc), set_hydrus_server_url(app, HYDRUS_SERVER_URL), set_session(
+    app, session
+):
+    if __name__ == "__main__":
+        # this is run only if development server is run
+        # Set the name of the API
+        socketio.run(app=app, debug=True, port=PORT)
+    else:
+        # Start the Hydrus app
+        http_server = WSGIServer(("", PORT), app)
+        logger.info(f"Running server at port {PORT}")
+        try:
+            http_server.serve_forever()
+        except KeyboardInterrupt:
+            pass
