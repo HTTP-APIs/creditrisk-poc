2af0671473de0055d65fcf15ca7a9ee044e27745
Amal Thundiyil <amalthundiyil0@gmail.com>
2022-03-07 16:37:29 +0530

    Add Integration tests with agent (#37)
    * Restructure folder
    * Add tests with `agent`
    * Add tests and Restructure
    * Minor changes

--- examples/mock_portfolio_generator.py
+++ examples/mock_portfolio_generator.py
@@ -1,7 +1,7 @@
 """
 Generates the mock portfolio data with all the FR key relations to the database.
 """
-from conftest import gen_dummy_object
+from tests.conftest import gen_dummy_object
 import json
 from hydra_python_core import doc_maker
 from hydra_python_core.doc_writer import HydraDoc
@@ -10,13 +10,17 @@ import os
 from os.path import join, dirname
 
 
-def get_api_doc(apidoc_file_path: str = "ApiDoc.jsonld") -> HydraDoc:
+def get_api_doc(apidoc_file_path: str = "mock_ApiDoc.jsonld") -> HydraDoc:
     """
     Returns HydraDoc object from API Documentation
     """
     path = join(dirname(__file__), apidoc_file_path)
     doc_file = open(path, "r")
-    API_NAME = os.environ["API_NAME"] if "API_NAME" in dict(os.environ).keys() else "creditrisk_api"
+    API_NAME = (
+        os.environ["API_NAME"]
+        if "API_NAME" in dict(os.environ).keys()
+        else "creditrisk_api"
+    )
     apidoc = json.load(doc_file)
     apidoc = doc_maker.create_doc(apidoc, "http://localhost:8080/", API_NAME)
     return apidoc, API_NAME
@@ -28,8 +32,12 @@ def generate_private_borrowers(apidoc: HydraDoc, API_NAME: str):
     """
     for borrower in range(1000):
         dummy_object = gen_dummy_object("PrivateBorrower", apidoc)
-        put_request = requests.put("http://localhost:8080/{api_name}/PrivateBorrower/".format(api_name=API_NAME),
-                                   json=dummy_object)
+        put_request = requests.put(
+            "http://localhost:8080/{api_name}/PrivateBorrower/".format(
+                api_name=API_NAME
+            ),
+            json=dummy_object,
+        )
 
 
 def generate_residential_mortgage(apidoc: HydraDoc, API_NAME: str):
@@ -39,8 +47,12 @@ def generate_residential_mortgage(apidoc: HydraDoc, API_NAME: str):
     for mortgage in range(100):
         # Creating ResidentialMortgage Object
         mortgage_dummy_object = gen_dummy_object("ResidentialMortgage", apidoc)
-        put_request = requests.put("http://localhost:8080/{api_name}/ResidentialMortgage/".format(api_name=API_NAME),
-                                   json=mortgage_dummy_object)
+        put_request = requests.put(
+            "http://localhost:8080/{api_name}/ResidentialMortgage/".format(
+                api_name=API_NAME
+            ),
+            json=mortgage_dummy_object,
+        )
 
 
 def generate_propertycollateral(apidoc: HydraDoc, API_NAME: str):
@@ -50,8 +62,12 @@ def generate_propertycollateral(apidoc: HydraDoc, API_NAME: str):
     for collateral in range(100):
         # Creating ResidentialMortgage Object
         collateral_dummy_object = gen_dummy_object("PropertyCollateral", apidoc)
-        put_request = requests.put("http://localhost:8080/{api_name}/PropertyCollateral/".format(api_name=API_NAME),
-                                   json=collateral_dummy_object)
+        put_request = requests.put(
+            "http://localhost:8080/{api_name}/PropertyCollateral/".format(
+                api_name=API_NAME
+            ),
+            json=collateral_dummy_object,
+        )
 
 
 def generate_forbearance(apidoc: HydraDoc, API_NAME: str):
@@ -61,8 +77,10 @@ def generate_forbearance(apidoc: HydraDoc, API_NAME: str):
     for forbearance in range(100):
         # Creating ResidentialMortgage Object
         forbearance_dummy_object = gen_dummy_object("Forbearance", apidoc)
-        put_request = requests.put("http://localhost:8080/{api_name}/Forbearance/".format(api_name=API_NAME),
-                                   json=forbearance_dummy_object)
+        put_request = requests.put(
+            "http://localhost:8080/{api_name}/Forbearance/".format(api_name=API_NAME),
+            json=forbearance_dummy_object,
+        )
 
 
 def generate_externalcollection(apidoc: HydraDoc, API_NAME: str):
@@ -72,8 +90,12 @@ def generate_externalcollection(apidoc: HydraDoc, API_NAME: str):
     for externalcollection in range(100):
         # Creating ResidentialMortgage Object
         externalcollection_dummy_object = gen_dummy_object("ExternalCollection", apidoc)
-        put_request = requests.put("http://localhost:8080/{api_name}/ExternalCollection/".format(api_name=API_NAME),
-                                   json=externalcollection_dummy_object)
+        put_request = requests.put(
+            "http://localhost:8080/{api_name}/ExternalCollection/".format(
+                api_name=API_NAME
+            ),
+            json=externalcollection_dummy_object,
+        )
 
 
 def generate_enforcement(apidoc: HydraDoc, API_NAME: str):
@@ -83,8 +105,10 @@ def generate_enforcement(apidoc: HydraDoc, API_NAME: str):
     for enforcement in range(100):
         # Creating ResidentialMortgage Object
         enforcement_dummy_object = gen_dummy_object("Enforcement", apidoc)
-        put_request = requests.put("http://localhost:8080/{api_name}/Enforcement/".format(api_name=API_NAME),
-                                   json=enforcement_dummy_object)
+        put_request = requests.put(
+            "http://localhost:8080/{api_name}/Enforcement/".format(api_name=API_NAME),
+            json=enforcement_dummy_object,
+        )
         print(put_request.text)
 
 
